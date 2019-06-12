feb3fa5821133e95d03094445341c500847f8956
kjames <kevin.james@linuxacademy.com>
2019-06-12 08:57:07 -0700

    added some content for lessons and labs

---
+++ Labs/Chef_DK_Tools-Docker_Chef_Generators_Test_Kitchen_and_Community_Cookbooks/config.rb
@@ -0,0 +1,10 @@
+#This is a copy of the config.rb used in this lab.
+cookbook_path ['~/chef/cookbooks']
+local_mode true
+if File.basename($PROGRAM_NAME).eql?('chef') && ARGV[0].eql?('generate')
+        chefdk.generator.license = "all_rights"
+        chefdk.generator.copyright_holder = "Student Name"
+        chefdk.generator.email = "you@example.com"
+        chefdk.generator_cookbook = "~/generator/corp_origin"
+end
+
