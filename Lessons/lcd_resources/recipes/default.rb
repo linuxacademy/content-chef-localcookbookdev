e2f954d23923625920b9ec982e524eb8ac5172f9
kjames <kevin.james@linuxacademy.com>
2019-07-01 11:54:13 -0700

    added code for the lcd_resources Lesson.

---
+++ Lessons/lcd_resources/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook:: lcd_resources
+# Recipe:: default
+#
+# Copyright:: 2019, Student Name, All Rights Reserved.
+lcd_resources_site 'httpd' do
+  homepage '<h1>Welcome to our example website!</h1>'
+end
+
---
+++ Lessons/lcd_resources/resources/site.rb
@@ -0,0 +1,19 @@
+property :homepage, String, default: '<h1>Hello world!</h1>'
+
+action :create do
+  package 'httpd'
+
+  service 'httpd' do
+    action [:enable, :start]
+  end
+
+  file '/var/www/html/index.html' do
+    content new_resource.homepage
+  end
+end
+
+action :delete do
+  package 'httpd' do
+    action :delete
+  end
+end
