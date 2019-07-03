399eb45c08f82c4b2224e6c9efd01a49ebe2ecf7
kjames <kevin.james@linuxacademy.com>
2019-07-03 15:33:39 -0700

    aded code for the InSpec Lesson

---
+++ Lessons/lcd_inspec/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook:: lcd_inspec
+# Recipe:: default
+#
+# Copyright:: 2019, Student Name, All Rights Reserved.
+['net-tools','httpd'].each do |pkg|
+  package pkg do
+   action :install
+  end
+end
+
