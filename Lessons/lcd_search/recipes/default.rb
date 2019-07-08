ce1ec22225ed145df10a47ab381fc85b6801a202
kjames <kevin.james@linuxacademy.com>
2019-07-08 12:53:22 -0700

    added lcd_search lesson.  Added Environments added data_bag

---
+++ Lessons/lcd_search/recipes/default.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook:: lcd_search
+# Recipe:: default
+#
+# Copyright:: 2019, Student Name, All Rights Reserved.
+
+admins = []
+
+search(:admins, "gid:administrators").each do |admin|
+
+  group admin['gid']
+  login = admin["id"]
+  admins << login
+  home = "/home/#{login}"
+
+  user(login) do
+    uid admin['uid']
+    gid admin['gid']
+    shell admin['shell']
+    comment admin['comment']
+    home home
+    manage_home true
+  end
+end
+
